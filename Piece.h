c2b6c68cb355ad35755fe1a3267de6ea4c9a14c9
Yuphemism <38116946+Yuphemism@users.noreply.github.com>
2018-04-05 18:41:14 -0400

    setup

--- Piece.h
+++ Piece.h
@@ -2,5 +2,14 @@
 ECE 4122 Final Project - 4/5/2018
 **********************************/
 //Includes
+#include <string>
+#include "Board.h"
 
-
+class Piece
+{
+public:
+	std::string color;
+	int x, y;
+	Piece(int X, int Y, std:string Color);
+	void canMove();
+};
