78af9714556863092f5b17ae3ac3206f0964e516
Yuphemism <38116946+Yuphemism@users.noreply.github.com>
2018-04-05 18:40:13 -0400

    setup

--- Board.cpp
+++ Board.cpp
@@ -2,5 +2,12 @@
 ECE 4122 Final Project - 4/5/2018
 **********************************/
 //Includes
+#include "Board.h"
 
+Board() {
 
+}
+
+int[] isEmpty() {
+
+}
---
+++ Piece.cpp
@@ -0,0 +1,13 @@
+#include <Piece.h>
+std::string color;
+int x, y;
+
+Piece(int X, int Y, std:string Color) {
+	x = X;
+	y = Y;
+	color = Color;
+}
+
+void canMove() {
+
+}
